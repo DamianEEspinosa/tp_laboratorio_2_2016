084353d54dc2af3fd0b0a62a9b8ba88e06515d89
DamianEEspinosa <damian52@hotmail.com>
2016-11-30 07:07:20 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP3/Espinosa.Damian.2D.TP3/testsUnitarios/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesAbstractas;
+using EntidadesInstanciables;
+using Excepciones;
+
+namespace TestsUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DniNoNumericoTest()
+        {
+            try
+            {
+                string dni = "unodostres";
+                Alumno alumno = new Alumno(200, "pepe", "lopez", dni, Persona.ENacionalidad.Argentino, Gimnasio.EClases.Natacion);
+            }
+            catch (Exception e)
+            {                
+                Assert.IsInstanceOfType(e, typeof(NacionalidadInvalidaException));
+            }
+        }
+      
+    }
+}
---
+++ RecuperatoriosTP/TP3/Espinosa.Damian.2D.TP3/testsUnitarios/UnitTest2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesAbstractas;
+using EntidadesInstanciables;
+
+namespace TestsUnitarios
+{
+    [TestClass]
+    public class ListasGimnasioTest
+    {
+        Gimnasio gimnasioPrueba = new Gimnasio();
+
+        [TestMethod]
+        public void listaAlumnosTest()
+        {
+
+            Assert.IsNotNull(gimnasioPrueba.Alumnos);
+
+        }
+
+        [TestMethod]
+        public void listaInstructoresTest()
+        {
+            Assert.IsNotNull(gimnasioPrueba.Instructores);
+        }
+
+        [TestMethod]
+        public void listaJornadasTest()
+        {
+            Assert.IsNotNull(gimnasioPrueba.Jornada);
+        }
+    }
+}
